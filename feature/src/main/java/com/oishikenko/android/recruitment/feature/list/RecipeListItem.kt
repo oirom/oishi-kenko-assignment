bab584b2342b738af4f88cc6f5355545f570b8f9
oirom <yamauchi.morio853@mail.kyutech.jp>
2023-02-01 20:22:19 +0900

    issue #2 (junior): implement list item

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListItem.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListItem.kt
@@ -1,39 +1,103 @@
 package com.oishikenko.android.recruitment.feature.list
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.oishikenko.android.recruitment.data.model.CookingRecord
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Composable
 fun RecipeListItem(
     cookingRecord: CookingRecord
 ) {
-    Row(
+    val displayRecipeType : Map<String, String> = mapOf(
+        "soup" to "スープ",
+        "main_dish" to "主菜/主食",
+        "side_dish" to "副菜"
+    )
+
+    val simpleDateFormatWithHyphen = SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
+    val date : Date = simpleDateFormatWithHyphen.parse(cookingRecord.recordedAt) as Date
+    val simpleDateFormatWithSlash = SimpleDateFormat("yyyy/MM/dd hh:mm")
+    val displayRecordedAt: String = simpleDateFormatWithSlash.format(date)
+
+    Spacer(
+        modifier = Modifier.size(8.dp)
+    )
+    Surface(
+        shape = MaterialTheme.shapes.small,
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(
-                horizontal = 16.dp,
-                vertical = 8.dp,
-            ),
+            .padding(horizontal = 16.dp),
     ) {
-        AsyncImage(
-            model = cookingRecord.imageUrl,
-            contentDescription = cookingRecord.comment,
-            contentScale = ContentScale.Crop,
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .size(96.dp)
-                .clip(RoundedCornerShape(4.dp)),
-        )
+                .border(
+                    width = 1.dp,
+                    color = Color(0xFFDCE0E0),
+                    shape = RoundedCornerShape(8.dp),
+                )
+                .background(
+                    color = Color(0xFFFFFFFF),
+                )
+            ,
+        ) {
+            AsyncImage(
+                model = cookingRecord.imageUrl,
+                contentDescription = cookingRecord.comment,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(
+                        shape = RoundedCornerShape(
+                            topStart = 8.dp,
+                            topEnd = 0.dp,
+                            bottomStart = 8.dp,
+                            bottomEnd = 0.dp
+                        )
+                    )
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        horizontal = 8.dp,
+                        vertical = 12.dp,
+                    )
+            ) {
+                Text(
+                    text = displayRecipeType[cookingRecord.recipeType].toString(),
+                    color = Color(0xFF333333),
+                    fontSize = 14.sp, /* from Figma inspect *//* TODO: sp?? dp?? */
+                    fontWeight = FontWeight(700), /* from Figma inspect */
+                    /* TODO: fontFamily('Noto Sans JP') */
+                )
+                Text(
+                    text = displayRecordedAt,
+                    color = Color(0xFF676767),
+                    fontSize = 14.sp, /* from Figma inspect *//* TODO: sp?? dp?? */
+                    fontWeight = FontWeight(400), /* from Figma inspect */
+                    /* TODO: fontFamily('Noto Sans JP') */
+                )
+            }
+        }
     }
 }
 
