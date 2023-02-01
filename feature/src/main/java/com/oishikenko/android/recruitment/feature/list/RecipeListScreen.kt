15b550d517c92db17812fd4b2959cbfb339a03dd
oirom <yamauchi.morio853@mail.kyutech.jp>
2023-02-01 20:19:39 +0900

    issue #1 (junior): add header

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
@@ -1,20 +1,21 @@
 package com.oishikenko.android.recruitment.feature.list
 
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.consumedWindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -26,23 +27,45 @@ fun RecipeListScreen(
     viewModel: RecipeListViewModel = hiltViewModel()
 ) {
     val cookingRecords by viewModel.cookingRecords.collectAsStateWithLifecycle()
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                modifier = Modifier,
+
+    Scaffold { innerPadding ->
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                Text(text = stringResource(id = R.string.cooking_records_title))
+                Text(
+                    text = stringResource(id = R.string.cooking_records_title),
+                    color = Color(0xFF333333),
+                    fontSize = 20.sp, /* from Figma inspect *//* TODO: sp?? dp?? */
+                    fontWeight = FontWeight(700), /* from Figma inspect */
+                    /* TODO: fontFamily('Noto Sans JP') */
+                    modifier = Modifier
+                        .height(29.dp)
+                        .width(160.dp)
+                )
+                Spacer(
+                    modifier = Modifier.size(4.dp) /* from Figma inspect */
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.header_image),
+                    contentDescription = "header_image",
+                    modifier = Modifier
+                        .height(64.dp) /* from Figma inspect */
+                        .width(66.72.dp) /* from Figma inspect */
+                )
             }
-        }
-    ) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(innerPadding)
-                .consumedWindowInsets(innerPadding)
-        ) {
-            items(cookingRecords) {
-                RecipeListItem(it)
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(innerPadding)
+                    .consumedWindowInsets(innerPadding)
+                ) {
+                    items(cookingRecords) {
+                    RecipeListItem(it)
+                }
             }
         }
     }
