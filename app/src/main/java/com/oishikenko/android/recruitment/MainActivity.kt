7e1a2086f47069c6cd2aa54996cf2c41b1a01a8b
oirom <yamauchi.morio853@mail.kyutech.jp>
2023-02-02 12:50:16 +0900

    navigation demo

--- app/src/main/java/com/oishikenko/android/recruitment/MainActivity.kt
+++ app/src/main/java/com/oishikenko/android/recruitment/MainActivity.kt
@@ -3,7 +3,14 @@ package com.oishikenko.android.recruitment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.oishikenko.android.recruitment.feature.list.RecipeListScreen
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.oishikenko.android.recruitment.ui.theme.RecruitmentTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,8 +20,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RecruitmentTheme {
-                RecipeListScreen()
+                // RecipeListScreen()
+                App()
             }
         }
     }
+}
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "profile") {
+        composable("profile") { ProfileScreen(navController) }
+        composable("friends") { FriendsScreen() }
+    }
+}
+
+@Composable
+fun ProfileScreen(navController: NavHostController) {
+    Column {
+        Text(text = "profile screen")
+        Button(
+            onClick = {
+                navController.navigate("friends")
+            }
+        ) {
+            Text(text = "go to friends")
+        }
+    }
+}
+
+@Composable
+fun FriendsScreen() {
+    Text(text = "friends screen")
 }
