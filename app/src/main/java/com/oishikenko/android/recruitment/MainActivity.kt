b1193f3b2a4c6c270bf7df428701e94bbea3cb04
oirom <yamauchi.morio853@mail.kyutech.jp>
2023-02-02 21:04:58 +0900

    add back button to back to recipe list screen

--- app/src/main/java/com/oishikenko/android/recruitment/MainActivity.kt
+++ app/src/main/java/com/oishikenko/android/recruitment/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.oishikenko.android.recruitment.feature.list.RecipeDetailScreen
 import com.oishikenko.android.recruitment.feature.list.RecipeListScreen
 import com.oishikenko.android.recruitment.ui.theme.RecruitmentTheme
@@ -27,12 +29,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun RecipeAppNavHost() {
     val navController = rememberNavController()
-    NavHost(navController, startDestination = "recipe list") {
-        composable("recipe list") {
+
+    NavHost(navController, startDestination = "recipe_list") {
+        composable("recipe_list") {
             RecipeListScreen(
-                onNavigateToDetail = { navController.navigate("recipe detail") }
+                onNavigateToDetail = { navController.navigate("recipe_detail") }
             )
         }
-        composable("recipe detail") { RecipeDetailScreen() }
+        composable("recipe_detail") {
+            RecipeDetailScreen(
+                onNavigateToList = { navController.popBackStack() }
+            )
+        }
     }
 }
--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeDetailScreen.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeDetailScreen.kt
@@ -1,11 +1,80 @@
 package com.oishikenko.android.recruitment.feature.list
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.oishikenko.android.recruitment.feature.R
 
 @Composable
-fun RecipeDetailScreen() {
-    Text(
-        text = "RecipeDetailScreen"
-    )
+fun RecipeDetailScreen(
+    onNavigateToList: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier
+                    .height(56.dp),
+                backgroundColor = Color.White,
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            horizontal = 20.dp,
+                        ),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        modifier = Modifier
+                            .clickable(
+                                onClick = onNavigateToList
+                            ),
+                        tint = Color(0xFF333333)
+                    )
+                    Spacer(modifier = Modifier.size(20.dp))
+                    Text(
+                        text = stringResource(id = R.string.recipe_detail_title),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF333333)
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding)
+        ) {
+            Text(text = "test")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewRecipeDetailScreen() {
+    MaterialTheme {
+        RecipeDetailScreen(
+            onNavigateToList = {}
+        )
+    }
 }
